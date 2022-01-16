94d9565831c63c6bd727a6193147d3eba30ba7c6
Salma-akter <64032942+Salma-akter@users.noreply.github.com>
2022-01-16 14:50:35 +0600

    Update Testimonial.js
    Review page Improve

--- src/Pages/Testimonial/Testimonial.js
+++ src/Pages/Testimonial/Testimonial.js
@@ -1,22 +1,19 @@
 import React from "react";
 import { Grid } from '@mui/material';
 import "./Testimonial.css";
+import { Avatar } from "@material-ui/core";
 const Testimonial = ({ data }) => {
-  const { name, comment, imageURL } = data;
+  const { name, comment, image } = data;
   return (
     <div className="col-md-4 mb-4 box-area">
       <div className="card">
-        {/* <img src={images} alt="" className="card-img-top"/> */}
         <div className="card-body">
           <p>{comment}</p>
-          <h5>by {name}</h5>
           <Grid xs={6} md={4}>
-            <img
-              style={{ width: "50px", height: "50px", textAlign: "center" }}
-              src={imageURL}
-              roundedCircle
-              alt={imageURL}
-             />
+            <div style={{display: 'flex', justifyContent:"space-between" ,textAlign: 'center'}}>
+              <Avatar src={image} alt={name}> {name?.charAt(0)}</Avatar>
+              <h5 > {name}</h5>
+            </div>
           </Grid>
         </div>
       </div>
