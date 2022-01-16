704175cdf60f0620cca979fabc7f8c62026843ff
Salma-akter <64032942+Salma-akter@users.noreply.github.com>
2022-01-16 15:27:34 +0600

    Update Result.js
    improve result

--- src/Pages/Result/Result.js
+++ src/Pages/Result/Result.js
@@ -1,21 +1,58 @@
 import { Button } from "@material-ui/core";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import Header from "../../components/Header/Header";
 import "./Result.css";
 
-const Result = ({ name, score }) => {
+const Result = ({ name, score, category }) => {
   const history = useHistory();
+  const [userName, setUserName] = useState('');
+  const [userScore,setUserScore] = useState('');
+  const [questionsType, setQuestionType] = useState('');
+  const [isClick, setIsClick] = useState(false);
+
 
   useEffect(() => {
     if (!name) {
       history.push("/");
     }
+    else if(name){
+      setUserName(name)
+    }
   }, [name, history]);
+  useEffect(() => {
+    if(category){ 
+      setUserScore(score)
+    }
+  }, [score])
+  
+  useEffect(() => {
+    if(category){  
+      setQuestionType(category)
+    }
+  }, [category])
+
+  const examineeScore = {
+    yourScore: userScore,
+    yourName: userName,
+    category: questionsType
+  }
+  const handleSubmit = () => {
+  
+    fetch('http://localhost:8000/score', {
+      method: "POST",
+      headers: {
+          'content-type': 'application/json'
+      },
+      body: JSON.stringify(examineeScore)
+  })
+  setIsClick(true)
+  }
+ 
 
   return (
     <>
-    <Header/>
+    {/* <Header/> */}
     <div className="result">
       <span className="title">Final Score : {score}</span>
       <Button
@@ -27,6 +64,17 @@ const Result = ({ name, score }) => {
       >
         Go to homepage
       </Button>
+      <Button disabled={isClick} onClick={()=>handleSubmit()}>Save Your Score</Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        size="large"
+        style={{ alignSelf: "center", marginTop: 20 }}
+        href="/dashboard"
+      >
+        Feedback
+      </Button>
+
     </div>
     </>
   );
